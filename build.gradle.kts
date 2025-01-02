efaf4e28d2ede0c3e328e2a5a3ac078228ae89bf
AbeAzka <azka.jsiswanto@gmail.com>
2025-01-02 21:57:13 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 // Top-level build.gradle file
 
 buildscript {
+
     repositories {
         google()
         mavenCentral()
@@ -23,4 +24,12 @@ buildscript {
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}
+subprojects {
+    repositories {
+        google()               // Google's Maven repository
+        mavenCentral()         // Maven Central repository
+        maven { url = uri ("https://jitpack.io") }  // JitPack repository for GitHub-based dependencies
+        jcenter()              // JCenter repository (note: it's deprecated)
+    }
 }
